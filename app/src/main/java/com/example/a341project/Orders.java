e358cadd19c98a6a4e5ed1ac8daf254878a3ca29
fatean1 <51172100+fatean1@users.noreply.github.com>
2020-03-26 10:25:05 +0800

    complete restuarant part

--- app/src/main/java/com/example/a341project/Orders.java
+++ app/src/main/java/com/example/a341project/Orders.java
@@ -9,35 +9,99 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
 public class Orders extends AppCompatActivity {
-    boolean orderStatus= false;
-    String[] orderTitle ={"Oh! Pizza","order2","order3","order4","order5","order6"};
-    String[] orderDistance ={"5 km","...","...","...","...","..."};
-    String[] orderAmount={"$24","...","...","...","...","..."};
-    ListView orderlist;
+
+    ListView orderList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //writeSample();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
-        Intent temp=getIntent();
-        orderlist = (ListView)findViewById(R.id.orderlistview);
-        listview RL= new listview(this,orderTitle,orderDistance,orderAmount);//create new java object
-        orderlist.setAdapter(RL);
+        Intent lastIntent=getIntent();
+        final String status=lastIntent.getStringExtra("status");
+        final ArrayList<String> temp;
+        temp=loadData(status);
+        String[] orderTitle =new String[temp.size()];
+        String[] orderDistance =new String[temp.size()];
+        String[] orderAmount=new String[temp.size()];
+        for (int i=0; i<temp.size();i++){
+            String[] arr=temp.get(i).split(",");
+            orderTitle[i]="Order"+i;
+            orderAmount[i]=arr[arr.length-2];
+            orderDistance[i]="5 km";
+        }
 
-        orderlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+        orderList = (ListView)findViewById(R.id.orderlistview);
+        OrderListview RL= new OrderListview(this,orderTitle,orderDistance,orderAmount);//create new java object
+        orderList.setAdapter(RL);
+
+        orderList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {//check the list view button is clicked
-                if(id==0){
-                    Intent intent=new Intent(Orders.this,OrderDetail.class);
-                    startActivity(intent);
-                }// jump to Oh Pizza page
-                else {
-                    Toast.makeText(Orders.this,"Coming Soon !", Toast.LENGTH_LONG).show();
-                }//else comming soon
+                Intent intent=new Intent(Orders.this,OrderDetail.class);
+                intent.putExtra("position",position);
+                intent.putExtra("orderInfo",temp.get(position));
+                intent.putExtra("status",status);
+                startActivity(intent);
             }
         });// On item click listener
 
     }
-
+    //write same sample order manually
+    public void writeSample(){
+        String info="John,,7788922113,fries2,coke1,pizza1,-,$24,0\n";
+        try{
+            FileOutputStream fOut = openFileOutput("sample.txt",
+                    MODE_APPEND);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            osw.write(info);
+            osw.close();
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+        }
+    }
+    public ArrayList<String> loadData(String status){
+        FileInputStream in=null;
+        ArrayList<String> temp= new ArrayList<String>();
+        try{
+            in = openFileInput("sample.txt");
+            InputStreamReader inputStreamReader =new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String info;
+            while((info=bufferedReader.readLine())!=null){
+                String[] arr=info.split(",");
+                if(arr[(arr.length-1)].equals(status)) {
+                    temp.add(info);
+                }
+            }
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(in!=null){
+                try{
+                    in.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return temp;
+    }
 
 }
--- app/src/main/java/com/example/a341project/RestaurantIndex.java
+++ app/src/main/java/com/example/a341project/RestaurantIndex.java
@@ -5,7 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class RestaurantIndex extends AppCompatActivity {
 
@@ -13,9 +21,27 @@ public class RestaurantIndex extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_index);
+        TextView icomingNum=findViewById(R.id.uncheckedOrderNum);
+        TextView cancelledNum=findViewById(R.id.cancelledOrderNum);
+        TextView checkedNum=findViewById(R.id.checkedOrderNum);
+        icomingNum.setText(""+loadData("0").size());
+        cancelledNum.setText(""+loadData("2").size());
+        checkedNum.setText(""+loadData("1").size());
     }
-    public void showOrders(View view){
+    public void showIncomingOrders(View view){
         Intent intent=new Intent(this,Orders.class);
+        intent.putExtra("status","0");
+        startActivity(intent);
+    }
+
+    public void showCancelledOrders(View view){
+        Intent intent=new Intent(this,Orders.class);
+        intent.putExtra("status","2");
+        startActivity(intent);
+    }
+    public void showacceptedOrders(View view){
+        Intent intent=new Intent(this,Orders.class);
+        intent.putExtra("status","1");
         startActivity(intent);
     }
     public void notWorkNow(View view){
@@ -24,5 +50,34 @@ public class RestaurantIndex extends AppCompatActivity {
                 Toast.LENGTH_SHORT);
 
         toast.show();
+    }
+    public ArrayList<String> loadData(String status) {
+        FileInputStream in = null;
+        ArrayList<String> temp = new ArrayList<String>();
+        try {
+            in = openFileInput("sample.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String info;
+            while ((info = bufferedReader.readLine()) != null) {
+                String[] arr = info.split(",");
+                if (arr[(arr.length - 1)].equals(status)) {
+                    temp.add(info);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return temp;
     }
 }
